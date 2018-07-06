4a68e1883814eceddbc14973fee08ea116683a9b
jasonrwise77 <jasonrwise77@gmail.com>
2018-07-06 12:42:26 -0400

    Update hem-reset-manager.groovy

--- smartapps/jasonrwise77/hem-reset-manager.src/hem-reset-manager.groovy
+++ smartapps/jasonrwise77/hem-reset-manager.src/hem-reset-manager.groovy
@@ -1,5 +1,5 @@
 /**
- *  HEM Monthly Reset Manager
+ *  HEM Reset Manager
  *
  *  Copyright 2017
  *
@@ -14,68 +14,48 @@
  *
  *
  */
-
 definition(
-    name: "HEM Monthly Reset Manager",,
+    name: "HEM Reset Manager",
     namespace: "jasonrwise77",
-    author: "Jason Wise",
-    description: "Resets the HEM on a specified day/time of every month",
-    parent: "jasonrwise77:HEM Reset Manager",
+    author: "jasonrwise77",
+    description: "Resets the Energy Monitor Daily on a specified time",
     category: "My Apps",
-    iconUrl: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/icons/HEM%20Monthly.png",
-    iconX2Url: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/icons/HEM%20Monthly.png",
-    iconX3Url: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/icons/HEM%20Monthly.png"
-    )
+    iconUrl: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/icons/HEM%20Daily.png",
+    iconX2Url: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/icons/HEM%20Daily.png",
+    iconX3Url: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/icons/HEM%20Daily.png")
 
+/**********************************************************************************************************************************************/
 preferences {
-    section("Choose an Aeon HEM v1 to reset:") {
-        input(name: "meter", type: "capability.energyMeter", title: "Which Aeon HEM v1? (tap here)", description: null, required: true, submitOnChange: true)
-    }    
-    section("Reset Time of Day") {
-        input "time", "time", title: "At this time of day"
-    }    
-    section("Reset Day of Month") {
-        input "day", "number", title: "On this day of the month"
-    }
+	page(name: "main")
 }
 
+page name: "main"
+            def main() {
+                dynamicPage (name: "main", title: "", install: true, uninstall: true) { 
+                section("Hem Profiles",  uninstall: false){
+                app(name: "profiles daily", appName: "HEM Daily Reset", namespace: "jasonrwise77", title: "Create a new Daily Reset Profile", multiple: true,  uninstall: false)
+		app(name: "profiles monthly", appName: "HEM Monthly Reset", namespace: "jasonrwise77", title: "Create a new Monthly Reset Profile", multiple: true,  uninstall: false)
+                        }
+                    }
+}
+
+/************************************************************************************************************
+		Base Process
+************************************************************************************************************/
 def installed() {
-	log.debug "Aeon HEM v1 Reset Manager SmartApp installed, now preparing to schedule the first reset."
-}
-
-def updated() {
-	log.debug "Aeon HEM v1 Reset Manager SmartApp updated, so update the user defined schedule and schedule another check for the next day."
-	unschedule()
-    def scheduleTime = timeToday(time, location.timeZone)
-    def timeNow = now()
-    log.debug "Current time is ${(new Date(timeNow)).format("EEE MMM dd yyyy HH:mm z", location.timeZone)}"
-    log.debug "Scheduling meter reset check at ${scheduleTime.format("EEE MMM dd yyyy HH:mm z", location.timeZone)}"
-    schedule(scheduleTime, resetTheMeter)
-}
-
-def initialize() {
-	unschedule()
-    def scheduleTime = timeToday(time, location.timeZone)
-    def timeNow = now()
-    log.debug "Current time is ${(new Date(timeNow)).format("EEE MMM dd yyyy HH:mm z", location.timeZone)}"
-    scheduleTime = scheduleTime + 1 // Next day schedule
-    log.debug "Scheduling next meter reset check at ${scheduleTime.format("EEE MMM dd yyyy HH:mm z", location.timeZone)}"
-    schedule(scheduleTime, resetTheMeter)
-}
-
-def resetTheMeter() {
-    Calendar localCalendar = Calendar.getInstance(TimeZone.getDefault());
-    def currentDayOfMonth = localCalendar.get(Calendar.DAY_OF_MONTH);
-    log.debug "Aeon HEM v1 meter reset schedule triggered..."
-    log.debug "...checking for the day of month requested by the user"
-    log.debug "...the day of the month right now is ${currentDayOfMonth}"
-    log.debug "...the day the user requested a reset is ${day}"
-    if (currentDayOfMonth == day) {
-        log.debug "...resetting the meter because it's when the user requested it."
-        meter.resetMeter()
-    } else {
-        log.debug "...meter reset not scheduled for today because it's not when the user requested it."
-    }
-    log.debug "Process completed, now schedule the reset to check on the next day."
+	if (debug) log.debug "Installed with settings: ${settings}"
     initialize()
+}
+def updated() { 
+	if (debug) log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+def initialize() {
+		state.esProfiles = state.esProfiles ? state.esProfiles : []
+        def children = getChildApps()
+}
+
+def getProfileList(){
+		return getChildApps()*.label
 }
