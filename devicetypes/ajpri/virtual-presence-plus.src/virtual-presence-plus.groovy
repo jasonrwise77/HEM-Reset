6cf6a7cdcc36d037585bdf85918d0b3342f8ae36
jasonrwise77 <jasonrwise77@gmail.com>
2018-08-31 11:52:55 -0400

    Update virtual-presence-plus.groovy

--- devicetypes/ajpri/virtual-presence-plus.src/virtual-presence-plus.groovy
+++ devicetypes/ajpri/virtual-presence-plus.src/virtual-presence-plus.groovy
@@ -4,7 +4,7 @@
 
 
 metadata {
-        definition (name: "Virtual Presence Plus", namespace: "ajpri", author: "Austin Pritchett", ocfDeviceType: "x.com.st.d.mobile.presence", vid:"generic-arrival") {
+        definition (name: "Virtual Presence Plus", namespace: "ajpri", author: "Austin Pritchett", vid:"generic-arrival") {
         capability "Switch"
         capability "Refresh"
         capability "Presence Sensor"
