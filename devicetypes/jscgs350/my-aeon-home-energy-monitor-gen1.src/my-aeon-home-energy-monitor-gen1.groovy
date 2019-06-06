448c3e1715983704b0ee88d6ed1db1e6aea68094
jasonrwise77 <jasonrwise77@gmail.com>
2019-06-05 22:57:21 -0400

    Update my-aeon-home-energy-monitor-gen1.groovy

--- devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen1.groovy
+++ devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen1.groovy
@@ -55,7 +55,7 @@
  *
  */
 metadata {
-	definition (name: "My Aeon Home Energy Monitor Gen1", namespace: "jscgs350", author: "jscgs350", ocfDeviceType: "oic.d.smartplug", vid: "generic-switch-power-energy")
+	definition (name: "My Aeon Home Energy Monitor Gen1", namespace: "jscgs350", author: "jscgs350", ocfDeviceType: "x.com.st.d.energymeter", vid: "generic-switch-power-energy")
 	{
 		capability "Energy Meter"
 		capability "Power Meter"
