68534a138bbe962815a6fcb346605c7c0d03d4dd
jasonrwise77 <jasonrwise77@gmail.com>
2019-09-25 19:27:40 -0400

    Create link-switches-locks.groovy

---
+++ smartapps/jasonrwise77/link-switches-locks.src/link-switches-locks.groovy
@@ -0,0 +1,69 @@
+/**
+ *  Link Switches - Locks
+ *
+ *  Link state between a switch and a lock
+ *
+
+ */
+
+definition(
+	name: "Link Switches - Locks",
+	namespace: "jasonrwise77",
+	author: "Jason Wise",
+	parent: "jasonrwise77:Link Switches",
+	description: "Link state between a switch and a lock",
+	category: "Convenience",
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Solution/doors-locks.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Solution/doors-locks@2x.png"
+)
+
+preferences {
+	section("When This Switch Changes") {
+		input "switch1", "capability.switch", multiple: false, required: false
+	}
+	section("Lock This Lock") {
+		input "lock1", "capability.lock", multiple: false, required: false
+     }
+}    
+
+def installed()
+{   
+	subscribe(switch1, "switch.on", onHandler)
+	subscribe(switch1, "switch.off", offHandler)
+	subscribe(lock1, "lock.locked", lockedHandler)
+	subscribe(lock1, "lock.unlocked", unlockedHandler)
+}
+
+def updated()
+{
+	unsubscribe()
+	subscribe(switch1, "switch.on", onHandler)
+	subscribe(switch1, "switch.off", offHandler)
+	subscribe(lock1, "lock.locked", lockedHandler)
+	subscribe(lock1, "lock.unlocked", unlockedHandler)
+
+}
+
+def onHandler(evt) {
+	log.debug evt.value
+	log.debug "Locking lock: $lock1"
+	lock1.lock()
+}
+
+def offHandler(evt) {
+	log.debug evt.value
+	log.debug "Unlocking lock: $lock1"
+	lock1.unlock()
+}
+
+def lockedHandler(evt) {
+	log.debug evt.value
+	log.debug "Turning on switch: $switch1"
+   	switch1.on()
+}
+
+def unlockedHandler(evt) {
+	log.debug evt.value
+	log.debug "Turning off switch: $switch1"
+   	switch1.off()
+}
